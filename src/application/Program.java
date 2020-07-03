34fb63b7b295f090a50cfbfae8ebdd01cb4a4849
Nilson Roman <nilsonoroman@gmail.com>
2020-07-03 14:45:43 -0300

    DepartmentDao implementation and test

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,15 +27,15 @@ public class Program {
 		
 		list.forEach(System.out::println);
 		
-		System.out.println("\n=== TEST 3: seller findByAll ====");
+		System.out.println("\n=== TEST 3: seller findAll ====");
 		list = sellerDao.findAll();
 		
 		list.forEach(System.out::println);
 		
-		/*System.out.println("\n=== TEST 4: seller insert ====");
+		System.out.println("\n=== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted new id = " + newSeller.getId());*/
+		System.out.println("Inserted new id = " + newSeller.getId());
 		
 		System.out.println("\n=== TEST 5: seller update ====");
 		seller = sellerDao.findById(1);
